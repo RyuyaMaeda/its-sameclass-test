03d933bb34bed14f67c4c18a4dff1eed45c61cb0
uesaka-seiya <seiya.uesaka@esm.co.jp>
2020-04-22 14:12:24 +0900

    "println追加しただけ"

--- sample.java
+++ sample.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println("VScodeの'Git Graph'っていう拡張機能入れたら楽しかったのでオヌヌメ");
     }
 }
